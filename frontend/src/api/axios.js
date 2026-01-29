e76790e477b74ab04b8508e8a17102dbc5f0ef1a
udayasri-pagilla <udayasripagilla1873@gmail.com>
2026-01-29 11:35:18 +0530

    deploy ready

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,16 @@
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
-api.interceptors.request.use((req) => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
